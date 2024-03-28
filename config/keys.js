cd61cd7023e7a9af8e9685b79a6786f5787bb6f9
Amit <amitak3107@gmail.com>
2024-03-28 18:02:28 +0530

    add all backend api of devconnector

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://amitak3107:Eagle33@cluster0.sldlwwk.mongodb.net/?retryWrites=true&w=majority'
-}
+  mongoURI: 'mongodb+srv://amitak3107:Eagle33@cluster0.sldlwwk.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey:'secret'
+};
